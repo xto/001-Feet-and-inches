40c02119b432411fbadb1696b2f05cb1a1dab580
Xavier Tô <xavier.to.01@gmail.com>
2011-08-05 17:01:46 -0400

    Extracted the string convertion to the Length object

--- lib/length_converter.rb
+++ lib/length_converter.rb
@@ -4,46 +4,7 @@ class LengthConverter
     @output_string = ""
   end
 
-  def inches_to_string!
-    add_spacing   unless @length.inches == 0
-    if @length.inches == 1
-      @output_string << "#{@length.inches} inch"
-    elsif @length.inches > 1
-      @output_string << "#{@length.inches} inches"
-    end
-  end
 
-
-  def feet_to_string!
-    unless @length.feet == 0
-      if @length.feet == 1
-        @output_string << "#{@length.feet} foot"
-      elsif @length.feet > 1
-        @output_string << "#{@length.feet} feet"
-      end
-    end
-  end
-
-  def simplify
-    if @length.thirty_seconds_of_an_inch % 16 == 0
-      "#{@length.thirty_seconds_of_an_inch/16}/2 of an inch"
-    elsif @length.thirty_seconds_of_an_inch % 8 == 0
-      "#{@length.thirty_seconds_of_an_inch/8}/4 of an inch"
-    elsif @length.thirty_seconds_of_an_inch % 4 == 0
-      "#{@length.thirty_seconds_of_an_inch/4}/8 of an inch"
-    elsif @length.thirty_seconds_of_an_inch % 2 == 0
-      "#{@length.thirty_seconds_of_an_inch/2}/16 of an inch"
-    else
-      "#{@length.thirty_seconds_of_an_inch}/32 of an inch"
-    end
-  end
-
-  def thirty_seconds_of_an_inch_to_string!
-    unless @length.thirty_seconds_of_an_inch == 0
-      add_spacing
-      @output_string << simplify()
-    end
-  end
 
   def convert_to_string(decimal_length)
     @length=Length.new(decimal_length)
@@ -52,9 +13,5 @@ class LengthConverter
     inches_to_string!
     thirty_seconds_of_an_inch_to_string!
     @output_string
-  end
-
-  def add_spacing
-    @output_string << ", " unless @output_string.empty?
   end
 end
--- spec/length_converter_spec.rb
+++ spec/length_converter_spec.rb
@@ -1,43 +1,4 @@
 require_relative "spec_helper"
 
 describe "LengthConverter" do
-  before :each do
-    @length_converter = LengthConverter.new
-  end
-
-  it "should convert 1 to 1 inch" do
-    @length_converter.convert_to_string(1).should ==("1 inch")
-  end
-
-  it "should convert 2 to 2 inches" do
-    @length_converter.convert_to_string(2).should == ("2 inches")
-  end
-
-  it "should convert 12 inches to 1 foot" do
-    @length_converter.convert_to_string(12).should == ("1 foot")
-  end
-
-  it "should convert 0.03125 to 1/32 of an inch" do
-    @length_converter.convert_to_string(0.03125).should == ("1/32 of an inch")
-  end
-
-  it "should convert 73.0185 to 6 feet, 1 inch, 1/32 of an inch" do
-    @length_converter.convert_to_string(73.03125).should == ("6 feet, 1 inch, 1/32 of an inch")
-  end
-
-  it "should convert 0.0625 to 1/16 of an inch" do
-    @length_converter.convert_to_string(0.0625).should == ("1/16 of an inch")
-  end
-
-  it "should convert 0.125 to 1/8th of an inch" do
-    @length_converter.convert_to_string(0.125).should == ("1/8 of an inch")
-  end
-
-  it "should convert 0.25 to 1/4th of an inch" do
-    @length_converter.convert_to_string(0.25).should == ("1/4 of an inch")
-  end
-
-  it "should convert 0.5 to 1/2 of an inch" do
-    @length_converter.convert_to_string(0.5).should == ("1/2 of an inch")
-  end
 end
